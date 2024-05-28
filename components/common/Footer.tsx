cbd7dbec1dc139a58375ce711f4c088ecb005bdf
LionAB <lionelabatan@gmail.com>
2024-05-28 16:26:25 +0200

    Changement footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,19 +2,15 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
       <p>
-        Made with{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-        by{" "}
+      &copy; {new Date().getFullYear()} - Développé par{" "} 
         <a
           className="text-blue-500 hover:underline"
-          href=""
+          href="https://www.linkedin.com/in/lionel-abatan-3ab389127/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Lionel ABATAN
-        </a>
+        </a> .Tous droits réservés.
       </p>
     </footer>
   );
